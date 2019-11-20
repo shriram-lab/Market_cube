e9f5fefb0b714dca7703ff5abb95ae2e7bd0d164
shri <shri@gmail.com>
2019-11-20 10:07:58 +0530

    add polaris

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider, Button } from '@shopify/polaris';
+import logo from './logo.svg';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppProvider i18n={enTranslations}>
+        <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+      </AppProvider>
     </div>
   );
 }
